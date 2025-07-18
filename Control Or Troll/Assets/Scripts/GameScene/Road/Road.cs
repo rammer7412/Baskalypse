afcdbb6d429f4f61a50a15336f808a7531877095
0si0 <nicky0705@naver.com>
2025-07-18 19:59:47 +0900

    feat: switched type Road 구현, Jumper 구현

--- Control Or Troll/Assets/Scripts/GameScene/Road/Road.cs
+++ Control Or Troll/Assets/Scripts/GameScene/Road/Road.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public enum RoadType
 {
-    switchtype, rotatetype
+    switchtype, rotatetype, statictype
 };
 
 
@@ -12,16 +12,28 @@ public class Road : MonoBehaviour
 {
     public RoadType roadType;
 
-  
+    [SerializeField] private float rotateDuration = 3f;
+    [SerializeField] private float switchDuration = 3f;
+    [SerializeField] private float switchRange = 5f;
+
+
     void Start()
-{
-    if (roadType == RoadType.rotatetype)
     {
-        // Y축을 따라 360도 만큼 1초에 한 바퀴, 끝나면 다시 반복, 선형 속도
-        transform
-            .DORotate(new Vector3(0, 0, 360), 3f, RotateMode.LocalAxisAdd)
-            .SetLoops(-1, LoopType.Restart)
-            .SetEase(Ease.Linear);
+        if (roadType == RoadType.rotatetype)
+        {
+            // Y축을 따라 360도 만큼 1초에 한 바퀴, 끝나면 다시 반복, 선형 속도
+            transform
+                .DORotate(new Vector3(0, 0, 360), rotateDuration, RotateMode.LocalAxisAdd)
+                .SetLoops(-1, LoopType.Restart)
+                .SetEase(Ease.Linear);
+        }
+        else if (roadType == RoadType.switchtype)
+        {
+            // 왔다가 갔다가 반복
+            transform
+                .DOMoveX(switchRange, switchDuration)
+                .SetLoops(-1, LoopType.Yoyo)
+                .SetEase(Ease.Linear);
+        }
     }
-}
 }
