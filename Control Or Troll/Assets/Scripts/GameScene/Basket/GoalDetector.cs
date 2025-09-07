14ae8055636622d7b15b8538a4872b656ae83259
rammer7412 <coderammer7412@gmail.com>
2025-09-07 21:57:00 +0900

    Fix: Renewal Basket GOal detection sys.

--- Control Or Troll/Assets/Scripts/GameScene/Basket/GoalDetector.cs
+++ Control Or Troll/Assets/Scripts/GameScene/Basket/GoalDetector.cs
@@ -1,37 +1,55 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BasketGoal : MonoBehaviour
 {
-    private Coroutine successCoroutine;
+    [Header("직접 입력한 총 공 개수")]
+    public int totalBallCount = 3;
+
+    private HashSet<Collider2D> ballsInside = new HashSet<Collider2D>();
     private GameObject successPanel;
 
     void Awake()
     {
         successPanel = UIManager.Instance.successPanel;
     }
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Ball"))
+        if (other.CompareTag("Ball") && !ballsInside.Contains(other))
         {
-            successCoroutine = StartCoroutine(WaitForBallStay(other));
-
+            StartCoroutine(CheckBallStay(other));
         }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Ball") && successCoroutine != null)
+        if (other.CompareTag("Ball"))
         {
-            StopCoroutine(successCoroutine);
-            successCoroutine = null;
+            ballsInside.Remove(other);
         }
     }
 
-    IEnumerator WaitForBallStay(Collider2D ball)
+    IEnumerator CheckBallStay(Collider2D ball)
     {
         yield return new WaitForSeconds(2f);
-        Debug.Log("공이 2초 동안 바구니 안에 있었음 → 성공!");
-        successPanel.SetActive(true);
+
+        if (IsBallStillInside(ball))
+        {
+            ballsInside.Add(ball);
+            Debug.Log($"공 {ballsInside.Count}/{totalBallCount} 바구니에 들어옴");
+
+            if (ballsInside.Count >= totalBallCount)
+            {
+                Debug.Log("모든 공이 바구니 안에 2초간 머무름 → 성공!");
+                successPanel.SetActive(true);
+            }
+        }
+    }
+
+    bool IsBallStillInside(Collider2D ball)
+    {
+        return GetComponent<Collider2D>().bounds.Contains(ball.transform.position);
     }
 }
